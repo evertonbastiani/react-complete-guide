0e86d54258639e281bc545e1579fe7463e0cb8a7
everton.bastiani <everton.bastiani-ext@xpollens.com>
2023-01-11 10:38:24 -0300

    Task1: time to pratice

---
+++ src/components/Expenses.js
@@ -0,0 +1,15 @@
+import ExpenseItem from "./ExpenseItem";
+
+function Expenses(props){  
+  return(
+       <div>
+         <ExpenseItem title="Toilet Paper" amount={94.12} date = {new Date(2023, 1, 9)}></ExpenseItem>
+         <ExpenseItem title="New TV" amount={799.49} date = {new Date(2023, 1, 1)} ></ExpenseItem>
+         <ExpenseItem title="Car Insurance" amount={294.67} date = {new Date(2023, 1, 4)} ></ExpenseItem>
+         <ExpenseItem title="New Desk(wooden)" amount={450} date = {new Date(2023, 1, 3)} ></ExpenseItem>
+         <ExpenseItem title="React complete guide course" amount={39.90} date = {new Date(2023, 1, 3)} ></ExpenseItem>
+       </div>  
+  );
+}
+
+export default Expenses;
